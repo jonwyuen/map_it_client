f4f04ed86ae9848e0c9adc14151478ac85dac114
Jonathan Yuen <jonwyuen@gmail.com>
2017-10-31 19:23:34 -0700

    initial commit

---
+++ src/Map.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // componentDidMount() {
+  //   this.initMap();
+  // }
+
+  // initMap(lat, long, zoom) {
+  //   lat = lat || 15;
+  //   long = long || -15
+  //   let map = new google.maps.Map(this.map, {
+  //     center: {
+  //       lat: lat,
+  //       lng: long
+  //     },
+  //     zoom: zoom,
+  //     mapTypeId: google.maps.MapTypeId.HYBRID,
+  //     disableDefaultUI: true,
+  //     zoomControl: true,
+  //     streetViewControl: true,
+  //     rotateControl: true
+  //   });
+  //   map.setOptions({ minZoom: 3, maxZoom: 20 });
+  // }
+
+  render() {
+    const MyMapComponent = withScriptjs(
+      withGoogleMap(props => (
+        <GoogleMap defaultZoom={3} defaultCenter={{ lat: 10, lng: 10 }} />
+      ))
+    );
+    return (
+      <MyMapComponent
+        style={{ height: "100%" }}
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCq7BzFiy1D0t2BD3_K9eRHqK8I5uatyxg"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
+  }
+}
+
+export default Map;
---
+++ src/MapContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import Map from "./Map"
+
+class MapContainer extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <Map />
+    )
+  }
+}
+
+export default MapContainer
